88e92f7f0ff0041ccf0843c71d72a15af11a3896
AnlixyStudy <alex19mu9kg26@gmail.com>
2023-12-19 13:49:44 +0300

    3

--- mylocal/models.py
+++ mylocal/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField('Name',max_length=50)
     post=models.TextField('discription')
+    
     def __str__(self):
         return self.title
